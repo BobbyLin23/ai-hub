fc7bb9cec9dbf306599531a6bf1c69524ba824c9
BobbyLin23 <linzhangsheng23@gmail.com>
2024-12-21 13:19:20 +0800

    refactor: change project to monorepo

---
+++ apps/website/components/Navbar.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { LogIn } from 'lucide-vue-next'
+
+const isVisible = ref(false)
+
+onMounted(() => {
+  isVisible.value = true
+})
+</script>
+
+<template>
+  <Transition
+    enter-from-class="opacity-0 -translate-y-4"
+    enter-to-class="opacity-100 translate-y-0"
+    enter-active-class="transition duration-300"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0 -translate-y-4"
+    leave-active-class="transition duration-300"
+  >
+    <header v-show="isVisible" class="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div class="container flex h-14 items-center">
+        <div class="mr-4 flex">
+          <Logo />
+        </div>
+        <div class="flex-1 flex justify-end md:justify-center">
+          <ClientOnly>
+            <NavMenu />
+          </ClientOnly>
+        </div>
+        <div class="flex items-center space-x-2">
+          <ClientOnly>
+            <LangToggle />
+            <ModeToggle />
+            <LoginButton />
+          </ClientOnly>
+        </div>
+      </div>
+    </header>
+  </Transition>
+</template>
---
+++ apps/website/layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="min-h-screen w-full bg-background">
+    <Navbar />
+    <slot />
+    <Footer />
+  </div>
+</template>
